4573c7a9c644d81bafde45b7fa3726695ce771bd
leman ibrahimli <leman.ibrahimli23@gmail.com>
2022-03-25 03:27:59 +0400

    #update

--- pages/about.js
+++ pages/about.js
@@ -3,23 +3,9 @@ import NavbarForAllPage from "../components/Layout/components/AllHeader/NavbarFo
 import MobileMenu from "../components/Layout/components/AllHeader/MobileMenu";
 import About from "../components/Layout/components/About/About";
 import Footer from "../components/Layout/components/Footer/Footer";
-import { useSelector, useDispatch } from 'react-redux';
-import { incrementCount, decrementCount, resetCount } from '../store/actions';
 export default function about() {
-    const count = useSelector((state) => state.counter);
-    const dispatch = useDispatch();
     return (
         <div>
-            <h1>
-                Count: <span>{count}</span>
-            </h1>
-            <button
-                type="button"
-                className="btn"
-                onClick={() => dispatch(incrementCount())}
-            >
-                +1
-            </button>
             <NavbarForAllPage/>
             <MobileMenu/>
             <About/>
