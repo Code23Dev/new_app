19fb8ea36e9c8ab2bf9ffbf551b4210c43d4d78e
Yusif <mahirliy55@gmail.com>
2022-03-25 12:26:33 +0300

    Slider is done

--- pages/home.js
+++ pages/home.js
@@ -3,7 +3,8 @@ import MobileMenu from "../components/Layout/components/AllHeader/MobileMenu";
 import Footer from "../components/Layout/components/Footer/Footer";
 import MainForHome from "../components/Layout/components/MainForHome/MainForHome";
 
-export default function Home() {
+export default function Home({data}) {
+    console.log(data, 'dassag');
     return (
         <div>
             <NavbarForAllPage/>
